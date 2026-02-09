a5666018ed03f850240fc247f8ae341d5f36e2cb
Dhanashree Raut <daraut0203@gmail.com>
2026-02-10 00:09:20 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Importing Bootstap from modules
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+// Importing Components
+import Navbar from './components/Navbar';
+import News from './components/News';
+
+import React, { Component } from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+
+export default class App extends Component {
+  pageSize=5
+  render() {
+    return (
+      <Router>
+        <Navbar/>
+        <Switch>
+        
+          <Route exact  path="/science">
+            <News key="science"  country="us" pageSize={this.pageSize} category="science"/>
+          </Route>
+          <Route exact  path="/general">
+            <News key="general"  country="us" pageSize={this.pageSize} category="general"/>
+          </Route>
+          <Route exact  path="/business">
+            <News key="business"  country="us" pageSize={this.pageSize} category="business"/>
+          </Route>
+          <Route exact  path="/entertainment">
+            <News key="entertainment"  country="us" pageSize={this.pageSize} category="entertainment"/>
+          </Route>
+          <Route exact  path="/health">
+            <News key="health"  country="us" pageSize={this.pageSize} category="health"/>
+          </Route>
+           <Route exact  path="/sports">
+            <News  key="sports" country="us" pageSize={this.pageSize} category="sports"/>
+          </Route>
+           <Route exact  path="/technology">
+            <News  key="technology" country="us" pageSize={this.pageSize} category="technology"/>
+          </Route>
+
+          <Route exact  path="/about">
+              About
+          </Route> 
+
+          <Route exact  path="/">
+            Home
+          </Route> 
+        </Switch>
+      {/* <News country="de" pageSize={this.pageSize} category="technology"/> */}
+      </Router>
+    )
+  }
 }
 
-export default App;
+
---
+++ src/components/News.js
@@ -0,0 +1,89 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner'
+
+export default class News extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      artical: [] ,
+      loading : true,
+      page : 1,
+      totalResult:0,
+    }
+  }
+
+  loadNextNews = async(event ) =>{
+    
+    await this.setState({
+      page: this.state.page+1,
+      loading: true,
+    });
+
+    await this.updateNews(this.state.page , "next");
+  
+  }
+
+  loadPrevNews = async (event ) =>{
+
+    await this.setState({
+      page: this.state.page-1,
+      loading: true,
+    });
+
+    await this.updateNews(this.state.page, "prev");
+  }
+
+  updateNews = async ( page , action )=>{
+
+
+
+    if( action === "next" && page+1> Math.ceil(this.state.totalResult/this.props.pageSize)){
+
+    }else{
+       let apiUrl = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&page=${page}&pageSize=${this.props.pageSize}&apiKey=b13c9a484b654f3ba25e963f1789f853`
+      let responce = await fetch(apiUrl);
+      let parsedData = await responce.json()
+
+      console.log(parsedData)
+      await this.setState({
+          totalResult: parsedData.totalResults,
+          artical: parsedData.articles,
+          loading: false,
+      })    
+    }
+
+   
+  }
+
+  
+  async componentDidMount(){
+    this.updateNews(1);
+  }
+
+  render() {
+    return (      
+      <div className='container my-3'>
+        <h1 className='text-center m-3'>Infynie News - Top Headlines</h1>
+        {this.state.loading ? 
+        <div  className='text-center'><Spinner></Spinner></div>: 
+        <div className="row justify-content-center">
+          {this.state.artical.map( (news)=> {
+             return <div className="col-md-4" key={news.url}>
+                      <NewsItem  title={news.title?news.title.slice(0,40):''} desc={news.description?news.description.slice(0,60):""} source={news.source.name} author={news.author?news.author:"Unknown"} publishDate={ news.publishedAt} imgUrl={news.urlToImage} link= {news.url}/>
+                      </div>
+                })}
+          </div>
+        }
+
+        {/* Next and Preview Button */}
+        <div className='d-flex justify-content-between' >
+          <button id="prev-btn" disabled={this.state.page<=1} onClick={this.loadPrevNews} className="btn btn-primary">Previous</button>
+          <button disabled={this.state.page+1> Math.ceil(this.state.totalResult/this.props.pageSize)} id="nx-btn" onClick={this.loadNextNews} className="btn btn-primary">Next</button>
+        </div>
+       
+      </div>
+    )
+  }
+}
